ddea7c130543c127f561831a694df0887fcb1e5a
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-11-03 23:34:46 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,42 +1,11 @@
 # Exercise 1
 class Rectangle:
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    def area(self):
-        return self.length * self.width
-
-    def perimeter(self):
-        return 2 * (self.length + self.width)
-
+    pass
 
 # Exercise 2
 class Book:
-    def __init__(self, title, author, price):
-        self.title = title
-        self.author = author
-        self.price = price
-
-    def display(self):
-        return f"Title: {self.title}, Author: {self.author}, Price: ${self.price}"
-
+    pass
 
 # Exercise 3
 class ShoppingCart:
-    def __init__(self):
-        self.names = []   # only storing names
-        self.prices = []  # storing prices separately
-
-    def add_item(self, name, price):
-        self.names.append(name)
-        self.prices.append(price)
-
-    def total_price(self):
-        return sum(self.prices)
-
-    def show_items(self):
-        result = ""
-        for i in range(len(self.names)):
-            result += f"{self.names[i]}: ${self.prices[i]}\n"
-        return result.strip()  # handles last line \n
+    pass
